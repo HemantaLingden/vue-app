79d548c9ac8aca7a2d020794b8685c2c0ece36c3
ritolika <lingdenhemanta@gmail.com>
2023-02-13 21:38:52 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <router-link to="/page1">Page 1</router-link>
-    <router-link to="/page2">Page 2</router-link>
+    <router-link to="/vue-app/page1">Page 1</router-link>
+    <router-link to="/vue-app/page2">Page 2</router-link>
     <router-view />
   </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/vue-app/' : '/',
+  base: '/vue-app/',
   // publicDir: process.env.NODE_ENV === 'production' ? 'vue-app' : '/',
 })
